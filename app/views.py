4657dd0150aeb219fdfe8b914138386da032c9cd
Kartikey Singh <kartikeysinghc@gmail.com>
2022-04-30 16:36:29 +0530

    added API key as env variable

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+from MapSearch.settings import MAPS_API_KEY
+
 
 class GetNearbyPlaces(View):
 
     def get(self, request):
-        return render(request, 'nearby_places_base.html', {})
+
+        return render(request, 'nearby_places_base.html', {
+            'api_key': MAPS_API_KEY
+        })
